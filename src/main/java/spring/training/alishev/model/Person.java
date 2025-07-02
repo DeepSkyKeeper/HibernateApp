863d751192ccb71db56e3778ebf019533298f8f2
deepSkyKeeper <detinetsgh@gmail.com>
2025-07-02 08:08:25 +0300

    id sequence

--- src/main/java/spring/training/alishev/model/Person.java
+++ src/main/java/spring/training/alishev/model/Person.java
@@ -7,7 +7,9 @@ import jakarta.persistence.*;
 public class Person {
     @Id
     @Column(name="id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_generator_person")
+    @SequenceGenerator(name="seq_genereator_person",
+    sequenceName = "person_id_seq",allocationSize=1)
 private int id;
     @Column(name="name")
 private String name;
