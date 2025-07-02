a73f2d6e518e457a401dbeb7e625c48f57c2f02a
deepSkyKeeper <detinetsgh@gmail.com>
2025-07-02 07:45:23 +0300

    id identity

--- src/main/java/spring/training/alishev/model/Person.java
+++ src/main/java/spring/training/alishev/model/Person.java
@@ -1,15 +1,13 @@
 package spring.training.alishev.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="Person")
 public class Person {
     @Id
     @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 private int id;
     @Column(name="name")
 private String name;
